98c3499c5f9a19f1b39bbf9c1ec296b5d6a36dbf
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2023-12-06 18:13:45 +0800

    migrasi

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const db = require('./models/index.js');
-const User = require('./models/Pengguna.js')
+const User = require('./models/Pengguna.js');
 const rootRoutes = require('./routes/index.js');
 const PORT = process.env.PORT || 3000;
 
-<<<<<<< HEAD
 // const db = require('./config/db');
 const allRoute = require('./routes');
 
@@ -14,13 +13,11 @@ const allRoute = require('./routes');
 // 	console.log('Connected to MongoDB');
 // });
 
-=======
->>>>>>> parent of 6fbf689 (migrasi > mongodb)
 app.use(express.json());
 app.use(rootRoutes);
 
 db.sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log("server running on port : " + PORT);
-    });
+	app.listen(PORT, () => {
+		console.log('server running on port : ' + PORT);
+	});
 });
