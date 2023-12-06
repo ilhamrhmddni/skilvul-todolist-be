268552f19e745b8f0169f488c0acc16e188e5a28
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2023-12-06 21:35:43 +0800

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const app = express();
-const db = require('./models/index.js');
-const User = require('./models/Pengguna.js');
-const rootRoutes = require('./routes/index.js');
+const rootRoutes = require('./routes');
+
 const PORT = process.env.PORT || 3000;
 
-// const db = require('./config/db');
-const allRoute = require('./routes');
-
-// db.on('error', console.error.bind(console, 'Connection error:'));
-// db.once('open', () => {
-// 	console.log('Connected to MongoDB');
-// });
-
-app.use(express.json());
+app.get(express.json());
 app.use(rootRoutes);
 
-db.sequelize.sync().then(() => {
-	app.listen(PORT, () => {
-		console.log('server running on port : ' + PORT);
-	});
+app.listen(PORT, () => {
+	console.log('Berhasil Terhubung ke Server');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,10 @@
 const express = require('express');
-const router = express.Router();
-const verifyToken = require('../middleware/auth');
-const todoRouter = require('./todo-router');
-const authRouter = require('./auth-router');
-const userRouter = require('./user-router');
-
-<<<<<<< HEAD
-// const userRoute = require('./user-route');
-// const authRoute = require('./auth-route');
-// const todoRoute = require('./todo-route');
+const route = express.Router();
 
 route.get('/', (req, res) => {
-	res.json('Ini Route');
+	res.json('Bershasil Terhubung Ke Root Routes');
 });
 
-// route.use('/auth', authRoute);
-// route.use('/user', userRoute);
-// route.use('/todo', todoRoute);
-=======
-router.get('/', (req, res) => {
-    res.json({
-        pesan: "Selamat datang di server",
-        versi: "1.0",
-        dokumentasi: "Tautan ke dokumentasi Anda"
-    });
-});
+// route.use('/user', userRoutes);
 
-// Middleware untuk menangani kesalahan
-router.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-        pesan: 'Terjadi kesalahan server',
-        error: err.message
-    });
-});
->>>>>>> parent of 6fbf689 (migrasi > mongodb)
-
-router.use('/auth', authRouter);
-router.use('/todos', verifyToken, todoRouter);
-router.use('/user', verifyToken, userRouter);
-
-module.exports = router;
+module.exports = route;
